a448985d70e16501cb90b9539ae9056064abaf6c
sagar <ssagargupta7007@gmail.com>
2025-06-23 00:32:06 +0530

    swagger dependency

--- src/main/java/com/sagar/MovieBookingSystem/Security/SecurityConfig.java
+++ src/main/java/com/sagar/MovieBookingSystem/Security/SecurityConfig.java
@@ -30,7 +30,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf-> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated())
                 .sessionManagement(session -> session
